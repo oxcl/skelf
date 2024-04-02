f52fd823a8678fed23fb11b370f4b358fed4ba86
oxcl <oxcl@proton.me>
2024-04-02 16:46:33 +0330

    write some prototype code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("test")
+interface Unit<T> {
+  read : (input : Input) => Promise<T>;
+  write : (value : T,output : Output) => Promise<number>;
+  constraint? : () => boolean;
+}
+
+interface IHandle {
+  getOffset : () => Promise<number>;
+}
+interface IInputHandle {
+  read : (size : number) => Promise<Uint8Array>;
+}
+
+interface IOutputHandle {
+  write : (chunk : Uint8Array) => Promise<number>;
+}
+
+class InputHandle {
+
+}
+
+const byte = {
+  read(input : Input){
+    if(input.locked)
+      throw new SkelfError(`can't read a ReadableStream input when it's locked. awaiting a previous call to a
+                            Unit or a function that uses this stream might fix the issue.`);
+    const reader = input.getReader();
+    reader.read()
+  }
+} as Unit<number>
+
+class SkelfError extends Error {
+  constructor(msg : string){
+    super(`skelf: ${msg}`.replace(/\n +/," "));
+  }
+}
