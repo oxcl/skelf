42a54ce8ed95050eb72e78f76956da2a98e1bc12
oxcl <oxcl@proton.me>
2024-04-07 16:51:32 +0330

    add basic prototype for space class

--- src/index.ts
+++ src/index.ts
@@ -24,9 +24,85 @@ export interface IWritableStream extends IAbstractStream {
 
 export type Offset = number | string | [number,number] | { amount : number, unit : number};
 
+// simple wrapper around ArrayBuffer to keep track of bit size of the buffer (useful when working with bits)
+export class SkelfBuffer extends ArrayBuffer {
+  constructor(
+    size : any,
+    bitLength : number
+  ){
+    super(size);
+    this.#bitLength = bitLength;
+  }
+  #bitLength : number
+  get bitLength(){ return this.#bitLength };
+  get bitPadding(){ return this.byteLength*8 - this.#bitLength }
+}
+
 export interface Skelf<T> {
   read : (input : IReadableSpace | IReadableStream,offset? : Offset) => Promise<T>;
   write : (value : T,output : IWritableSpace | IWritableStream, offset? : Offset) => Promise<number>;
+}
+function convertToUint8Array(input : ArrayBuffer | Uint8Array){
+  if(input instanceof Uint8Array)
+    return input;
+  else
+    return new Uint8Array(input);
+}
+export class Space {
+  #locked : boolean = false;
+  readonly _close? : () => Promise<void>;
+  readonly _read : (size : number, offset : number) => Promise<ArrayBuffer>;
+  get locked(){ return this.#locked};
+  async close(){
+    if(this.locked)
+      throw new LockedSpaceError(`
+        trying to close space '${this.name}' while it's locked. this could be caused by a not awaited call
+        to this method, which might be still pending.
+      `)
+    if(this._close) = await this._close();
+  }
+  async read(size : Offset, offset : Offset){
+    if(this.locked)
+      throw new LockedSpaceError(`
+        trying to read from space '${this.name}' while it's locked. this could be caused by a not awaited
+        call to this method, which might be still pending.
+      `)
+    this.#locked = true;
+    const totalBitsToOffset = offsetToBits(offset); // convert offset to bits
+    const wholeBytesToOffset = Math.floor(totalBitsToOffset / 8); // calculate offset in whole bytes
+    const leftoverBitsToOffset = totalBitsToOffset % 8; // calculate the leftover offset bits
+    const sizeInBits = offsetToBits(size); // convert size to bits
+    const bytesToRead = Math.ceil((leftoverBitsToOffset + sizeInBits) / 8) // how many bytes to cover all bits
+    const bitShift = (bytesToRead*8 - (leftoverBitsToOffset + sizeInBits)) % 8; // how many bits to shift right
+
+    const buffer = await this._read(bytesToRead,wholeBytesToOffset);
+    const uint8 = convertToUint8Array(buffer);
+
+    // zero out the beginning of the first byte which doesn't include the desired bits
+    uint8[0] &= 0xFF >> leftoverBitsToOffset
+
+    if(bitShift !== 0){
+      // shift the bits into
+      let leftoverOfPrevByte = 0, leftoverOfThisByte = 0;
+      for(let i = 0; i < uint8.length; i++) {
+        // get the bits that will be cut from the right of the this byte to add them to the next byte later
+        leftoverOfThisByte = uint8[i] & (0xFF >> (8-bitShift));
+        // shift the byte by bitShift to the right
+        uint8[i] >>= bitShift;
+        // add the left overs from the previous byte that was shifted to this byte
+        uint8[i] |= leftoverOfPrevByte << (8-bitShift);
+        leftoverOfPrevByte = leftoverOfThisByte;
+      }
+    }
+
+    this.#locked = false;
+    // if the size doesn't have leftover bits but the offset does. that means after shifting bits to correct
+    // positions, there should be a redundant empty byte at the beginning of the buffer that was read.
+    if(bitShift + leftoverBitsToOffset >= 8){
+      return new SkelfBuffer(uint8.slice(1).buffer);
+    }
+    return new SkelfBuffer(buffer.buffer);
+  }
 }
 
 export * from "skelf/units"
--- src/playground.ts
+++ src/playground.ts
@@ -1,4 +1,4 @@
-import {ISpace,IReadableSpace,Offset} from "skelf"
+import {ISpace,IReadableSpace,Offset,SkelfBuffer} from "skelf"
 import units from "skelf/units"
 import {InvalidOffsetError,LockedSpaceError} from "skelf/errors"
 import * as fs from "node:fs/promises"
@@ -48,48 +48,6 @@ async function openFile(fileName : string) : Promise<ISpace> {
     get locked(){ return this.#locked}
 
     async read(size : Offset,offset : Offset = 0){
-      if(this.locked)
-        throw new LockedSpaceError(`
-          trying to read from space '${this.name}' while it's locked. this could be caused by a not awaited
-          call to this method, which might be still pending.
-        `)
-      this.#locked = true;
-      const totalBitsToOffset    = offsetToBits(offset); // convert offset to bits
-      const wholeBytesToOffset   = Math.floor(totalBitsToOffset / 8); // calculate offset in whole bytes
-      const leftoverBitsToOffset = totalBitsToOffset % 8; // calculate the leftover offset bits
-      const sizeInBits = offsetToBits(size); // convert size to bits
-      const bytesToRead   = Math.ceil((leftoverBitsToOffset + sizeInBits) / 8) // bytes to cover all bits
-      const bitShift = (bytesToRead*8 - (leftoverBitsToOffset + sizeInBits)) % 8; // how many bits to shift
-
-      // read the part of the space that will contain all the needed bits
-      const buffer = new Uint8Array(bytesToRead);
-      await file.read({
-        buffer : buffer,
-        position: wholeBytesToOffset
-      })
-      // zero out the beginning of the first byte which doesn't include the desired bits
-      buffer[0] &= 0xFF >> leftoverBitsToOffset
-
-      if(bitShift !== 0){
-        let leftoverOfPrevByte = 0, leftoverOfThisByte = 0;
-        for(let i = 0; i < buffer.length; i++) {
-          // get the bits that will be cut from the right of the this byte to add them to the next byte later
-          leftoverOfThisByte = buffer[i] & (0xFF >> (8-bitShift));
-          // shift the byte by bitShift to the right
-          buffer[i] >>= bitShift;
-          // add the left overs from the previous byte that was shifted to this byte
-          buffer[i] |= leftoverOfPrevByte << (8-bitShift);
-          leftoverOfPrevByte = leftoverOfThisByte;
-        }
-      }
-
-      this.#locked = false;
-      // if the size doesn't have leftover bits but the offset does. that means after shifting bits to correct
-      // positions, there should be a redundant empty byte at the beginning of the buffer that was read.
-      if(bitShift + leftoverBitsToOffset >= 8){
-        return buffer.slice(1).buffer;
-      }
-      return buffer.buffer;
     }
     async write(chunk : ArrayBuffer, offset : Offset = 0){
       if(this.locked)
@@ -102,17 +60,14 @@ async function openFile(fileName : string) : Promise<ISpace> {
       const wholeBytesToOffset = Math.floor(totalBitsToOffset); // calculate offset in whole byte
       const leftoverBitsToOffset = totalBitsToOffset % 8; // calculate the leftover offset bits
 
-
-
+      // since you can't write from the middle of a byte, if the offset has bit leftovers then we have to first
+      // read the byte in the middle to add the bits that we want to it while keeping the previous bits.
       if(leftoverBitsToOffset !== 0){
+        this.#locked = false;
+        const middleByte = (await this.read(1,wholeBytesToOffset))[0];
 
+        this.#locked = true;
       }
-
-
-      // if leftoverBitsToOffset is not zero then we have to first read one byte
-    }
-    async close(){
-      await file.close();
     }
   }
 }
