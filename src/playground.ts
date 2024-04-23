de68b7e3facb7fa67b81261078ff62ba7567d6cb
oxcl <oxcl@proton.me>
2024-04-23 16:50:58 +0330

    implement the prototype version of createStruct

--- src/playground.ts
+++ src/playground.ts
@@ -1,12 +1,31 @@
-import {int,int8,uint,ArraySpace,float32,float64,cstring,dynamicString} from "skelf";
+import {int,int8,uint,ArraySpace,float32,float64,cstring,dynamicString,createStruct} from "skelf";
 import {NodeFileSpace} from "skelf/node"
-
 
 const space = await new NodeFileSpace("test.txt").init();
 
-const dString = dynamicString(int8);
+const personStruct = createStruct("person",{
+  name: dynamicString(int8),
+  age : int8
+})
 
-await dString.write("my name is ali",space);
-console.log(await dString.read(space))
+const companyStruct = createStruct("company",{
+  name : dynamicString(int8),
+  type : int8,
+  body : (struct)=> Math.random() > 0.5 ? dynamicString(int8) : int8,
+  ceo: personStruct,
+  rank: int8
+})
+
+//const company = {
+//  name : "alfa",
+//  type : int8,
+//  ceo : {
+//    name : "ronagh",
+//    age : 5
+//  },
+//  rank: 1
+//}
+//
+//await companyStruct.write(company,space);
 
 await space.close();
---
+++ src/struct.ts
@@ -0,0 +1,42 @@
+import {ISkelfDataType} from "skelf/types"
+import {createDataType} from "skelf/data_type"
+
+type Distribute<T> = T extends any ? ISkelfDataType<T> : never
+type Object = {[k: string]: any}
+type StructSchema<T extends Object> = {
+  [k in keyof T]: ISkelfDataType<T[k]> | ((struct : any) => Distribute<T[k]>)
+}
+
+export function createStruct<T extends Object>(name : string,schema : StructSchema<T>){
+  return createDataType<T>({
+    name,
+    async read(reader){
+      const object : any = {};
+      for(const [property,value] of Object.entries(schema)){
+        const dataType = (typeof value === "function") ? value(object as Partial<T>) : value;
+        object[property] = await dataType.read(reader);
+      }
+      return object as T;
+    },
+    async write(writer,object){
+      for(const [property,value] of Object.entries(schema)){
+        const dataType = (typeof value === "function") ? value(object as Partial<T>) : value;
+        await dataType.write(object[property],writer);
+      }
+    }
+  })
+}
+
+export default createStruct;
+
+//
+//createStruct("struct",{
+//  name : cstring,
+//  age  : int(8)
+//})
+//
+//
+//{
+//  name : ISkelfDataType<string>,
+//  age : ISkelfDataType<number>
+//}
